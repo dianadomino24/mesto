c52f50aae08518309463082576ecad3a1d41d5e0
Diana Vlasenkova <dianadomino24work@gmail.com>
2020-07-17 19:14:02 +0300

    6.3.1 all review fixed except data attributes

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,3 @@
-const popup = document.querySelectorAll('.popup')
 const popupProfileOpenButton = document.querySelector('.profile__edit-button')
 const popupCloseButton = document.querySelector('.popup__close-button')
 const profileName = document.querySelector('.profile__name')
@@ -9,15 +8,9 @@ const inputJob = document.querySelector('.popup__input_type_job')
 //сюда будем класть текущий открытый попап
 let currentPopupBox = null
 
-//imgTriggers у меня перезаписывается при удалении/добавлении карточек places, 
-//поэтому я не могу сделать его const.
-//или Вы хотите, чтобы я не перезаписывала массив, а написала отдельные ф-ции, которые
-//бы нашли в массиве, какую именно картинку надо удалить и таким образом обновили массив? 
-// а при добавлении карточки, добавили в массив его картинку?
-let imgTriggers = document.querySelectorAll('[data-img-trigger]')
 const imgPopup = document.querySelector('.popup_type_picture-zoom')
 
-let popupTriggers = document.querySelectorAll('[data-popup-trigger]')
+const popupTriggers = document.querySelectorAll('[data-popup-trigger]')
 
 const placesList = document.querySelector('.places__list')
 const placeForm = document.querySelector('.popup__form_type_place')
@@ -36,15 +29,16 @@ function keyHandler(evt) {
     }
 }
 //открывает/закрывает текущий попап из коробки
-const popupToggle = function () {
+function popupToggle() {
     currentPopupBox.classList.toggle('popup_opened')
-    //прослушка для кнопок, чтоб закрыть при Esc
-    document.addEventListener('keydown', keyHandler)
 
     //опустошает currentPopupBox при закрытии попапа и убирает прослушку Esc
     if (currentPopupBox && !currentPopupBox.classList.contains('popup_opened')) {
         currentPopupBox = null
         document.removeEventListener('keydown', keyHandler)
+    } else {
+        //прослушка для кнопок, чтоб закрыть при Esc
+        document.addEventListener('keydown', keyHandler)
     }
 }
 
@@ -57,25 +51,48 @@ function cleanInputValues() {
 }
 
 //закрывает попап при нажатии на фон
-const closePopupByClickingOverlay = function (event) {
+function closePopupByClickingOverlay(event) {
     if (event.target !== event.currentTarget) { return }
     
     cleanInputValues()
     popupToggle()
 }
 
-//прослушки для закрытия текущего попапа
-function addPopupListeners(currentPopupBox) {
-    currentPopupBox.querySelector('.popup__close-button').addEventListener('click', popupToggle);
-    currentPopupBox.addEventListener('click', closePopupByClickingOverlay);
+
+const popupList = Array.from(document.querySelectorAll('.popup'))
+//прослушки для закрытия открытого попапа
+function addPopupListeners(popupList) {
+    popupList.forEach(popup => {
+        popup.querySelector('.popup__close-button').addEventListener('click', popupToggle)
+        popup.addEventListener('mousedown', closePopupByClickingOverlay)
+    })
 }
+
+addPopupListeners(popupList)
+
+
+//если открылся попап редактирования профиля, то разблокируем его кнопку сабмита (чтоб она была доступна до изменения инпутов)
+//для попапа добавл.карточек заблокируем кнопку
+function submitButtonStateBeforeInput(currentPopupBox) {
+    if (currentPopupBox && currentPopupBox.classList.contains('popup_type_edit-profile')) {
+        currentPopupBox.querySelector('.popup__save-button').classList.remove('popup__save-button_disabled')
+    }
+    if (currentPopupBox && currentPopupBox.classList.contains('popup_type_add-place')) {
+        currentPopupBox.querySelector('.popup__save-button').classList.add('popup__save-button_disabled')
+    }
+}
+
+// //прослушки для закрытия текущего попапа
+// function addPopupListeners(currentPopupBox) {
+//     currentPopupBox.querySelector('.popup__close-button').addEventListener('click', popupToggle);
+//     currentPopupBox.addEventListener('mousedown', closePopupByClickingOverlay);
+// }
+
 //открывает текущий попап, ставит прослушки и очищает input values и скрывает уведомления об ошибках
 function openCurrentPopup(evt) {
     const currentTrigger = evt.target
     const currentPopupValue = currentTrigger.dataset.popupTrigger
 
-    //ответ к комментарию "можно лучше": в этот момент еще нет попапа с классом opened, 
-    // т.к. я сначала по дата атрибуту нахожу попап, а потом уже делаю ему тогл :)
     currentPopupBox = document.querySelector(`[data-popup-name=${CSS.escape(currentPopupValue)}]`)
 
     cleanInputValues()
@@ -87,12 +104,10 @@ function openCurrentPopup(evt) {
     //чтобы после открытия попапа при нажатии Enter не срабатывал toggle(фокус сместится с кнопки-триггера)
     document.activeElement.blur()
 
-    //если открылся попап редактирования профиля, то разблокируем его кнопку сабмита (чтоб она была доступна до изменения инпутов)
-    if (currentPopupBox && currentPopupBox.classList.contains('popup_type_edit-profile')) {
-        currentPopupBox.querySelector('.popup__save-button').classList.remove('popup__save-button_disabled')
-    }
+    //установит состоянии кнопки при открытии попапа до введения инпута
+    submitButtonStateBeforeInput(currentPopupBox)
 
-    addPopupListeners(currentPopupBox)
+    // addPopupListeners(currentPopupBox)
 }
 
 //при клике на триггер запускает открытие соответствующего триггеру попапа
@@ -118,9 +133,6 @@ function profileFormSubmitHandler (evt) {
 }
 
 
-//не придумала, как обойтись полностью без этой ф-ции, т.к. при сабмите на вход я получаю форму, 
-// и не могу исп-ть ее напрямую в hasInvalidInput, сначала надо найти inputList в форме.
-
 //проверка валидности формы с учетом пробелов
 function isFormInvalid(form) {
     inputList = Array.from(form.querySelectorAll('.popup__input'))
@@ -137,53 +149,49 @@ formProfile.addEventListener('submit', function(evt) {
 }
 );
 
-
-// удаляет карточку
-function deletePlace(e) {
-    const place = e.target.closest('.places__item');
-    place.remove();
-    updateImgTriggers ();
-}
-
-//прослушки для кнопок лайка и удаления карточек
-function addPlaceListeners(place) {
-    place.querySelector('.place__like-button').addEventListener('click', function(evt) {
-        evt.target.classList.toggle('place__like-button_active');
-    }) 
-    place.querySelector('.place__delete-button').addEventListener('click', deletePlace)
-}
-
 // проверяет, есть ли в списке картинки, если нет, то делает видимой надпись о пустом списке
+//в placesList всегда есть минимум 1 элемент - надпись о пустом списке
 function checkEmptyPlacesList() {
-    if (imgTriggers.length === 0) {
+    if (placesList.children.length === 1) {
         emptyList.classList.add('places__empty-list_visible')
     } else {
         emptyList.classList.remove('places__empty-list_visible')
     }
 }
 
-// обновляет массив карточек и проверяет пустой ли список(все карточки удалены)
-function updateImgTriggers () {
-    imgTriggers = document.querySelectorAll('[data-img-trigger]')
-
+// удаляет карточку
+function deletePlace(e) {
+    const place = e.target.closest('.places__item');
+    place.remove();
     checkEmptyPlacesList()
-
-    imgTriggers.forEach(trigger => trigger.addEventListener('click', openImgPopup))
 }
 
-// добавляет карточку, ставит ей прослушки и обновляет массивы с карточками и изобр.-триггерами
-function addPlace(placeName , placePic) {
+//прослушки для кнопок лайка и удаления карточек
+function addPlaceListeners(place) {
+    //прослушка для картинки, чтоб открыть попап с картинкой
+    const placeImgTrigger = place.querySelector('.place__image')
+    placeImgTrigger.addEventListener('click', openImgPopup)
+
+    place.querySelector('.place__like-button').addEventListener('click', function(evt) {
+        evt.target.classList.toggle('place__like-button_active');
+    }) 
+    place.querySelector('.place__delete-button').addEventListener('click', deletePlace)
+}
+
+// создает карточку, ставит ей прослушки
+function createPlace(placeName , placePic) {
     const place = placeTemplate.content.cloneNode(true);
 
     place.querySelector('.place__name').textContent = placeName
     place.querySelector('.place__image').src = placePic
 
     addPlaceListeners(place)
-    
-    placesList.prepend(place)
-    updateImgTriggers ()  
+    return place
 }
 
+function addPlace(placesList, place) {
+    placesList.prepend(place)
+}
 
 function placeFormSubmitHandler (evt) {
     evt.preventDefault();
@@ -191,11 +199,11 @@ function placeFormSubmitHandler (evt) {
     const placeName = placeInputName.value
     const placePic = placeInputPic.value
 
-    addPlace(placeName, placePic);
+    const place = createPlace(placeName, placePic);
     
-    //после добавления карточки очищаем инпуты
-    placeInputName.value = '';
-    placeInputPic.value = '';
+    addPlace(placesList, place)
+
+    checkEmptyPlacesList() 
 
     popupToggle()
 }
@@ -209,9 +217,10 @@ placeForm.addEventListener('submit', function(evt) {
     evt.preventDefault()
 }) 
 
-// добавит начальные карточки из массива
+// добавит начальные карточки из массива (initialCards в utils.js)
 initialCards.forEach(element => {
-    addPlace(element.name, element.link)
+    const place = createPlace(element.name, element.link);
+    addPlace(placesList, place)
 }) 
 
 // откроет попап с приближенной картинкой, исходя из триггер-картинки
@@ -227,8 +236,6 @@ function openImgPopup(evt) {
     currentPopupBox = imgPopup
 
     popupToggle()
-    addPopupListeners(currentPopupBox)
+    // addPopupListeners(currentPopupBox)
 }
-
-imgTriggers.forEach(trigger => trigger.addEventListener('click', openImgPopup))
 
